6a0d79e712c41383797c87e091a05df38c160608
ArpitTandon1995 <arpit.tndon0505@gmail.com>
2023-07-17 14:50:42 +0530

    initial commit

---
+++ src/custom_exception/VehicleValidationException.java
@@ -0,0 +1,10 @@
+package custom_exception;
+@SuppressWarnings("serial")
+public class VehicleValidationException extends Exception {
+	public VehicleValidationException(String mesg) {
+		super(mesg);
+	}
+}
+
+
+  
